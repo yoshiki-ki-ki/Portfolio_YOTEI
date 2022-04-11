ed590d01b7278fb72e1aff2269ac633f102b0155
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-04-11 13:41:44 +0000

    [Update]routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,23 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   get '/manual' => 'homes#manual'
+  
   resources :schedules, only: [:show]
   resources :schedule_answers, only: [:new, :create, :edit, :update, :destroy]
-  
+
   namespace :host do
-    get 'events/new'
-    post 'events/create'
+    get '/mypage' => 'homes#index'
+    
+    post '/events/event_cofirmation'
+    resources :events, only: [:new, :create]
 
-    get 'homes/index'
-
-    get 'event_schedules/show'
-    get 'event_schedules/schedule_confirmation'
-    patch 'event_schedules/update'
+    get '/host/event_schedules/:id/schedule_confirmation' => 'event_schedules#schedule_confirmation'
+    resources :event_schedules, only: [:show, :update]
   end
 
-
-
-  devise_for :hosts
+  devise_for :hosts, controllers: {
+  registrations: "host/registrations",
+  sessions: 'host/sessions'
+}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
