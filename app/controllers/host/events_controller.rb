f2ee507900ab8aa79741c5cef743abaf271a8bd9
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-04-12 10:07:39 +0000

    [Update]events_controller

--- app/controllers/host/events_controller.rb
+++ app/controllers/host/events_controller.rb
@@ -1,9 +1,17 @@
 class Host::EventsController < ApplicationController
   before_action :authenticate_host!
-
+  
   def new
+    @event = Event.new
   end
 
   def create
   end
+  
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :schedule_time, :comment, :decision_date_start, :decision_date_end)
+  end
+  
 end
