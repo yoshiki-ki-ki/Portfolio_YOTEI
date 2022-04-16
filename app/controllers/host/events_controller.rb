98d93e20096319570512ff5354237673a71f0ce7
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-04-16 09:22:13 +0000

    [Update]event_paramsに複数の子レコードを送信

--- app/controllers/host/events_controller.rb
+++ app/controllers/host/events_controller.rb
@@ -1,12 +1,18 @@
 class Host::EventsController < ApplicationController
   before_action :authenticate_host!
-  
+
   def new
     @event = Event.new
+    3.times do
+      @event.event_schedules.build
+    end
   end
-  
-  def event_confirmation
+
+  def event_cofirmation
+    byebug
     @event = Event.new(event_params)
+    @event.host_id = current_host.id
+    @event_info = Event.find(params[:id])
   end
 
   def create
@@ -15,11 +21,11 @@ class Host::EventsController < ApplicationController
     @event.save
     redirect_to post_images_path
   end
-  
+
   private
 
   def event_params
     params.require(:event).permit(:title, :schedule_time, :comment, event_schedules: [:candidate_date_start, :candidate_date_end])
   end
-  
+
 end
