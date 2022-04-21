22d1e1996f7db5e5c01bfb128e4e17a251537407
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-04-21 10:49:15 +0000

    [Update]イベント回答ページ

--- app/controllers/schedule_answers_controller.rb
+++ app/controllers/schedule_answers_controller.rb
@@ -1,10 +1,32 @@
 class ScheduleAnswersController < ApplicationController
   def new
+    @event = Event.find(params[:event_id])
+  end
+
+  def answer_confirmation
+
+  end
+
+  def create
+
   end
 
   def answer
   end
 
+  def answer_pass
+
+  end
+
   def edit
   end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,21 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   get '/manual' => 'homes#manual'
-  
+
   get '/schedules/:event_id' => 'schedules#show' , as: 'schedule'
-  resources :schedule_answers, only: [:new, :create, :edit, :update, :destroy]
+
+  get '/schedule_answers/:event_id/new' => 'schedule_answers#new' , as: 'new_schedule_answer'
+  post '/schedule_answers/:event_id/answer_confirmation' => 'schedule_answers#answer_confirmation' , as: 'answer_confirmation_schedule_answer'
+  post '/schedule_answers/:event_id' => 'schedule_answers#answer_confirmation' , as: 'create_schedule_answer'
+  get '/schedule_answers/:event_id/pass' => 'schedule_answers#answer' , as: 'answer_schedule_answer'
+  post '/schedule_answers/:event_id/pass' => 'schedule_answers#answer_pass' , as: 'answer_pass_schedule_answer'
+  get '/schedule_answers/:event_id/:id/edit' => 'schedule_answers#edit' , as: 'edit_schedule_answer'
+  post '/schedule_answers/:event_id/:id' => 'schedule_answers#update' , as: 'update_schedule_answer'
+  post '/schedule_answers/:event_id/:id' => 'schedule_answers#destroy' , as: 'destroy_schedule_answer'
 
   namespace :host do
     get '/mypage' => 'homes#index'
-    
+
     post '/events/event_cofirmation'
     resources :events, only: [:new, :create]
 
