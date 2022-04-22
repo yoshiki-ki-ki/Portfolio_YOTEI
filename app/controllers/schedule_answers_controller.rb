edcf612604a154add9c7a81b07f5a05f3f407039
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-04-22 04:40:13 +0000

    [Update]ゲスト回答

--- app/controllers/schedule_answers_controller.rb
+++ app/controllers/schedule_answers_controller.rb
@@ -1,32 +1,39 @@
 class ScheduleAnswersController < ApplicationController
   def new
+    @guest = Guest.new
     @event = Event.find(params[:event_id])
   end
 
   def answer_confirmation
-
+    @guest = Guest.new(guest_params)
+    @event = Event.find(params[:event_id])
   end
 
   def create
-
+    @guest = Guest.new(guest_params)
+    @event.save
+    redirect_to schedule_path(@event.id)
   end
 
   def answer
   end
 
   def answer_pass
-
   end
 
   def edit
   end
 
   def update
-
   end
 
   def destroy
+  end
 
+  def guest_params
+    params.require(:guest).permit(
+      :guest_name, :comment_to_host, :password,
+      guest_schedules_attributes: [:id, :join_flag, :_destroy])
   end
 
 end
--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,4 +1,6 @@
 class Guest < ApplicationRecord
   belongs_to :event
   has_many :guest_schedules, dependent: :destroy
+  
+  accepts_nested_attributes_for :guest_schedules, reject_if: :all_blank, allow_destroy: true
 end
