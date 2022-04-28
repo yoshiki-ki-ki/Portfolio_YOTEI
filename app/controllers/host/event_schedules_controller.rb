7b11758364da7e1b965eecc85fdf838668936a3a
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-04-28 12:13:59 +0000

    [Update]参加者の回答内容の表示

--- app/controllers/host/event_schedules_controller.rb
+++ app/controllers/host/event_schedules_controller.rb
@@ -2,6 +2,7 @@ class Host::EventSchedulesController < ApplicationController
   def show
     @event_schedule = EventSchedule.find(params[:id])
     @guests = @event_schedule.event.guests.all
+    @guest_schedules = GuestSchedule.where(event_schedule_id: params[:id])
   end
 
   def schedule_confirmation
--- app/controllers/schedule_answers_controller.rb
+++ app/controllers/schedule_answers_controller.rb
@@ -8,20 +8,33 @@ class ScheduleAnswersController < ApplicationController
     @guest = Guest.new(guest_params)
     @event = Event.find_by(token: params[:token])
     session[:guest_schedules] = params[:guest][:guest_schedules].to_unsafe_h
+    @guest_schedules = []
+    session[:guest_schedules].each_with_index do |guest_schedule_params,i|
+      guest_schedule = GuestSchedule.new
+      guest_schedule.guest_id = @guest.id
+      guest_schedule.event_schedule_id = guest_schedule_params[1]["parent_schedule"].to_i
+      guest_schedule.join_flag = guest_schedule_params[1]["join_flag"]
+
+      @guest_schedules.push(guest_schedule)
+    end
   end
 
   def create
     @guest = Guest.new(guest_params)
     @event = Event.find_by(token: params[:token])
     @guest.event_id = @event.id
+
     @guest.save
-    session[:guest_schedules].each do |guest_schedule_params|
+    @guest_schedules = []
+    session[:guest_schedules].each_with_index do |guest_schedule_params,i|
       guest_schedule = GuestSchedule.new
       guest_schedule.guest_id = @guest.id
       guest_schedule.event_schedule_id = guest_schedule_params[1]["parent_schedule"].to_i
       guest_schedule.join_flag = guest_schedule_params[1]["join_flag"]
       guest_schedule.save
+      @guest_schedules.push(guest_schedule)
     end
+    # @guest_schedules = GuestSchedule.where([:guest_id][:event_schedule_id])
     redirect_to schedule_path(token: @event.token)
   end
 
@@ -41,15 +54,20 @@ class ScheduleAnswersController < ApplicationController
   def edit
     @guest = Guest.find(params[:id])
     @event = @guest.event
+    @guest_schedules = @guest.guest_schedules
   end
 
   def update
+    @guest = Guest.find(params[:id])
+    @guest.update(guest_params)
+    @event = @guest.event
+    redirect_to schedule_path(token: @event.token)
   end
 
   def destroy
     @guest = Guest.find(params[:id])
     @guest.destroy
-    @event = Event.find_by(token: params[:token])
+    @event = @guest.event
     redirect_to schedule_path(token: @event.token)
   end
 
