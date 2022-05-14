6d8e0c6c419bb4bfa0a33fd9672d03ea1309e5e2
yoshiki-ki-ki <98401400+yoshiki-ki-ki@users.noreply.github.com>
2022-05-14 00:17:27 +0000

    [Fix]マイページの不具合修正

--- app/controllers/host/homes_controller.rb
+++ app/controllers/host/homes_controller.rb
@@ -1,6 +1,6 @@
 class Host::HomesController < ApplicationController
   def index
-    @events = Event.page(params[:page]).order(created_at: :desc)
-    # @events = Event.find(current_host.id)
+    # @events = Event.page(params[:page]).order(created_at: :desc)
+    @events = Event.where(host_id: current_host.id).page(params[:page]).order(created_at: :desc)
   end
 end
